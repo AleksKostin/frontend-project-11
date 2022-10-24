c2260f53d9ff4971bf199854f47878cc610b8003
Aleksey Kostin <alekkostin91@gmail.com>
2022-10-24 16:49:18 +0300

    fix: rules eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-underscore-dangle */
+
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import { fileURLToPath } from 'url';
